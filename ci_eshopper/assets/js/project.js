7317739e3a3ad10d314c72be4279b58cda5c64cc
kajalyerunkar <37833204+kajalyerunkar@users.noreply.github.com>
2018-08-17 22:13:17 +0530

    Add files via upload

---
+++ ci_eshopper/assets/js/project.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+	//console.log(curl)
+
+	$.get(curl+ "get_categories", function(data, status){
+
+		//console.log(status)
+		if(status=="success"){
+
+			//console.log(JSON.parse(data))
+		}
+	});
+
+	$.get(curl+ "get_brands", function(data, status){
+
+		
+		//console.log(status)
+		if(status=="success"){
+
+			//console.log(data)
+
+			$(".brands_data").html(data)
+
+		}
+	});
+
+	$.get(curl+ "get_products", function(data, status){
+
+		//console.log(status)
+		if(status=="success"){
+
+			//console.log(data)
+
+			$(".features_items").html(data)
+
+		}
+	});
+
+	$(document).on("click",".br_data",function(aobj){
+
+		aobj.preventDefault();
+		var id= $(this).attr("for")
+		//alert(id);
+
+		//alert("brid="+id)
+		$.ajax({
+			type:"post",
+			data:"brid="+id,
+			url: curl+"get_products_brandwise",
+			success:function(response){
+
+				//alert(response);
+				$(".features_items").html(response)
+
+			}
+		})
+
+
+
+	})
+})
