e528514ca7f4649bde677a48597596c40a0b0a5d
kajalyerunkar <37833204+kajalyerunkar@users.noreply.github.com>
2018-08-30 21:32:25 +0530

    Add files via upload

--- application/controllers/Project.php
+++ application/controllers/Project.php
@@ -85,19 +85,20 @@
 
 		//echo "TEST";
 
-		print_r($_POST);
+		//print_r($_POST);
 		$data= $this ->input->post('proid');
 		//echo($data);
 
 		$result = get_cookie("cartproduct");
 
-		print_r($result);
+		//print_r($result);
 		//exit;
 
 		if($result == ""){
 
 			set_cookie("cartproduct",$data,time()+3600, "","/");
-			echo "product Added";
+			$msg= "product Added";
+			$cnt= 1;
 		}
 
 		else{
@@ -114,16 +115,50 @@
 
 			if($pos){
 
-				echo "product exist in cart";
+				$msg= "product exist in cart";
+				$cnt = count(explode(",",$result));
 			}
 			else{
 
 				$newdata = $result.",".$data;
 				set_cookie("cartproduct",$newdata,time()+3600,"","/");
-				echo "product updated";
+
+				$msg ="product updated";
+				$cnt = count(explode(",", $newdata));
+
 			}
 		}
+
+		echo $cnt."#".$msg;
 	}
+
+	public function get_cart_products(){
+
+		if(get_cookie("cartproduct") && get_cookie("cartproduct")!=""){
+
+			$data = get_cookie("cartproduct");
+
+
+		$this->load->model('project_model');
+
+		$ans = $this->project_model->getAllProductsCart($data);
+
+		if(is_array($ans)){
+			$str="";
+
+			foreach($ans as $val){
+
+				// print_r($val);
+
+				$str = $str ."<div class='col-sm-4'><div class='product-image-wrapper'><div class='single-products'><div class='productinfo text-center'><img src='".base_url().$val->p_imgpath."' alt='' /><h2>".$val->p_amt."</h2><p>".$val->p_name."</p><a href='#' class='btn btn-default add-to-cart'><i class='fa fa-shopping-cart'></i>Add to cart</a></div><div class='product-overlay'><div class='overlay-content'><h2>$56</h2><p>Easy Polo Black Edition</p><a href='#' class='btn btn-default add-to-cart' for='".$val->p_id."'><i class='fa fa-shopping-cart'></i>Add to cart</a></div></div></div><div class='choose'><ul class='nav nav-pills nav-justified'><li><a href='#'><i class='fa fa-plus-square'></i>Add to wishlist</a></li><li><a href='#'><i class='fa fa-plus-square'></i>Add to compare</a></li></ul></div></div></div>";
+			}
+			echo $str;
+		}
+	}
+	else{
+			echo "no product in cart";
+	}
+}
 
 }
 
--- application/models/Project_model.php
+++ application/models/Project_model.php
@@ -34,6 +34,22 @@
 				return 0;
 			}
 		}
+
+		function getAllProductsCart($rec){
+
+			$str = "select p_id,p_name,p_amt,p_discount,p_caid,
+			p_brid,p_desc,p_imgpath,ca_name,br_name from category, brand,products where ca_id = p_caid and br_id=p_brid and p_id in($rec) order by p_id desc";
+
+			$result = $this->db->query($str);
+
+			if($result->result_id->num_rows>0){
+
+				return $result->result();
+			}
+			else{
+				return 0;
+			}
+		}
 	}
 
 ?>
